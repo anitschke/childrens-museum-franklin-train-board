42d4df91104091cb4706397605a01e57b116b2f3
Andy Nitschke <andrew.b.nitschke@gmail.com>
2025-10-19 20:42:59 -0400

    Adding comment on async HTTP requests

--- code.py
+++ code.py
@@ -39,8 +39,35 @@ import gc
 # data. The data that we get from these json files is MASSIVE, I think it will
 # be way too much for us to parse into json and dig through all that data to
 # find what we were looking for.
-#
-#
+
+
+
+# xxx doc The esp32-s3 comes with a co-processor for handling HTTP requests. So
+# ideally we would send out the HTTP request to get updated arrival times and
+# have the co-processor wait for the response. While we are waiting for the
+# response the main processor can continue doing other tasks like animating the
+# board. The idea here is that I could use async / await along with asyncio
+# python library to do cooperative multitasking (
+# https://learn.adafruit.com/cooperative-multitasking-in-circuitpython-with-asyncio/overview
+# ). The main processor can just keep checking in to see if the co-processor has
+# finished receiving the HTTP response. 
+# 
+# Unfortunately the CircuitPython Requests library that we can use for sending
+# HTTP requests on the esp32-s3 Matrix Portal board does not have async method
+# that we can use to send HTTP requests. There is an issue about this on GitHub,
+# I did some investigation into this. It would be possible to add this
+# functionality but it would be a lot of work because it would requiring making
+# the low level socket using for sending the HTTP request async in order to add
+# a way for the processor to wait for the coprocessor to receive response at the
+# correct point in time. This is just too much work for the train board that I
+# am working on.
+# https://github.com/adafruit/Adafruit_CircuitPython_Requests/issues/134#issuecomment-3415845378 
+# 
+# xxx doc As a result we need to work around this issue. So we need to be
+# careful about when we send the HTTP request so that it isn't in the middle of
+# an animation or something.
+
+
 
 # xxx The https://api-v3.mbta.com/vehicles API will also give us "live" (seems
 # like it updates every 10s or so) data on vehicles such as lat/lon, speed,
