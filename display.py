60a8236348f7beb383c2edb7298121a3a517571f
Andy Nitschke <andrew.b.nitschke@gmail.com>
2025-11-02 21:28:58 -0500

    cleaning up graphics

--- display.py
+++ display.py
@@ -29,8 +29,23 @@ class Display:
         self._train_frame_duration = train_frame_duration
         
         self._arrival_time_indices = None
-    
+
+        # xxx doc important we init train after arrival times so it shows up on top
+        self._initialize_arrival_times()
+        self._initialize_train()
+        gc.collect()
+    def _set_mode(self, mode):
+        if mode == DisplayMode.ARRIVAL_TIMES:
+            self._train_sprite_group.hidden = True
+        if mode == DisplayMode.TRAIN:
+            self._train_sprite_group.hidden = False
+        self._mode = mode
+
+
+
     def render_arrival_times(self, trains):
+        self._set_mode(DisplayMode.ARRIVAL_TIMES)
+
         assert(len(trains)== 3, "expecting three train objects to be provided to render_arrival_times")
 
         # xxx there is an issue where after the train animation plays the screen
@@ -43,9 +58,6 @@ class Display:
         # group and we deleted that as part of clearing the board to show the train.
         # 
         # see https://github.com/adafruit/Adafruit_CircuitPython_PortalBase/blob/25fc43dd67ae95a8e62173e90c3069502194873a/adafruit_portalbase/graphics.py#L51
-
-        if self._mode != DisplayMode.ARRIVAL_TIMES:
-            self._initialize_arrival_times()
 
         times = [self._format_train_time(t) for t in trains]
 
@@ -60,9 +72,6 @@ class Display:
 
 
     def _initialize_arrival_times(self):
-        self._matrix_portal.remove_all_text()
-
-        self._matrix_portal.set_background('/background.bmp')
         self._matrix_portal.add_text( text_font=ARRIVAL_TIMES_FONT, text_position=(15, 3), text="Children's Museum of Franklin", is_data=False, scrolling=True)
         
         self._arrival_time_indices = [
@@ -70,30 +79,30 @@ class Display:
             self._matrix_portal.add_text( text_font=ARRIVAL_TIMES_FONT, text_position=(16, 19), text="?min", is_data=False),
             self._matrix_portal.add_text( text_font=ARRIVAL_TIMES_FONT, text_position=(16, 27), text="?min", is_data=False),
         ]
+
+        # When we add the tLogo on the left side of the screen we want the
+        # scrolling "Children's Museum of Franklin" to scroll UNDER the T
+        # instead of on top of it. With displayio items added to a group last
+        # stack on TOP of other layers. So we need to add the tLogo AFTER the
+        # text so it stacks on top of the text. We also set the third color
+        # palette color as transparent so the text can scroll partly on top of
+        # the logo.
+        tLogo = displayio.OnDiskBitmap('/background.bmp')
+        palette = tLogo.pixel_shader
+        palette.make_transparent(3)
+        tLogoSprite = displayio.TileGrid(
+                tLogo,
+                pixel_shader=palette,
+            )
+        self._matrix_portal.display.root_group.append(tLogoSprite)
+
         self._mode = DisplayMode.ARRIVAL_TIMES
+        gc.collect()
 
     def scroll_text(self):
         self._matrix_portal.scroll_text(self._text_scroll_delay)
-
-    def render_train(self, direction):
-        self._mode = DisplayMode.TRAIN
-        self._render_train(direction)
-
-        # After we have rendered the train replace the root group to make sure
-        # we remove any existing train animation and then run the GC to free up
-        # all the memory from the animation.
-        self._matrix_portal.display.root_group = displayio.Group()
-        gc.collect()
     
-    def _render_train(self, direction):
-        self._matrix_portal.remove_all_text()
-
-        # Now that wae have removed all text replace the root group to make sure
-        # there is nothing else being displayed.
-        sprite_group = displayio.Group()
-        self._matrix_portal.display.root_group = sprite_group
-        gc.collect()
-
+    def _initialize_train(self):
         WIDTH=64
         HEIGHT=32
 
@@ -105,21 +114,27 @@ class Display:
             tile_height=HEIGHT,
         )
 
+        self._train_sprite_group = displayio.Group()
+        self._train_sprite_group.append(sprite)
+        self._matrix_portal.display.root_group.append(self._train_sprite_group)
+
+        self._train_frame_count = int(bitmap.height / HEIGHT)
+
+    def render_train(self, direction):
+        self._set_mode(DisplayMode.TRAIN)
+
         # The train animation is setup for an outbound train by default. So if
         # we want to render an inbound train we need to flip the sprite
         if (direction == Direction.IN_BOUND):
-            sprite.flip_x = True
+            self._train_sprite_group.flip_x = True
 
-        sprite_group.append(sprite)
-
-        frame_count = int(bitmap.height / HEIGHT)
         current_frame = 0
         while True:
             time.sleep(self._train_frame_duration)
             current_frame = current_frame + 1
-            if current_frame >= frame_count:
+            if current_frame >= self._train_frame_count:
                 return
 
             # Advance to the next frame by using __setitem__ on the
             # sprite_group.
-            sprite_group[0][0] = current_frame
+            self._train_sprite_group[0][0] = current_frame
